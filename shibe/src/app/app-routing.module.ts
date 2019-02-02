8454e130a038ce9b2ef6f745919e8bb4793287e5
ink-pad <cagabour@edu.uwaterloo.ca>
2019-02-02 18:50:00 -0500

    Added boilerplate code

--- shibe/src/app/app-routing.module.ts
+++ shibe/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TaskListComponent } from './app-page/task-list/task-list.component';
+
+const routes: Routes = [
+  { path: '', component: TaskListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- shibe/src/app/app.module.ts
+++ shibe/src/app/app.module.ts
@@ -2,16 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TaskListComponent } from './app-page/task-list/task-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
