846dc98ebc74b03743f85af28a824ae68bb20ca3
Ella Cai <ella.y.cai@gmail.com>
2019-02-02 19:12:21 -0500

    added dogalog, shibe class, and game service

---
+++ shibe/src/app/dogalog/dogalog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dogalog',
+  templateUrl: './dogalog.component.html',
+  styleUrls: ['./dogalog.component.scss']
+})
+export class DogalogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ shibe/src/app/shibe.spec.ts
@@ -0,0 +1,7 @@
+import { Shibe } from './shibe';
+
+describe('Shibe', () => {
+  it('should create an instance', () => {
+    expect(new Shibe()).toBeTruthy();
+  });
+});
