ad6fe94c4c9c95a350891bb9090eaeed51327eaf
Michelle <michelle.gong@uwaterloo.ca>
2019-02-03 00:01:43 -0500

    wrote game functionaty

--- shibe/src/app/game/game.component.ts
+++ shibe/src/app/game/game.component.ts
@@ -64,3 +64,30 @@ let ownedToys: string[] = [
 
 //room status
 let expanded: boolean = false
+
+//poses
+//make objects here with pose name and left and top locations
+
+
+//function to draw ownedShibes
+function drawDogs() {
+  //clearss dogs usedPoses and sets setOfPoses to poses
+  document.getElementById('dog-container').innerHTML = "";
+  var usedPoses = [];
+  var setOfPoses = poses;
+  //loops through ownedDogs
+  for(var i = 0 ; i < ownedDogs.length ; i++ ) {
+    //(Math.random() * x) where x is # of poses -- randomly generates a poseID
+    var poseID = Math.Ceiling((Math.random() * setOfPoses.length))
+    //inserts doge at index i in ownedDogs in pose poseID
+    document.getElementById('dog-container').innerHTML += `<div class='dog' style='top:${pose[poseID].top}px; left:${pose[poseID].left}px;'><img src='../../assets/images/shibes/shibe${ownedDogs[i]}_${pose[poseID].name}'></div>`;
+    //appends poseID to usedPoses Array
+    usedPoses.push({
+          name: pose[poseID].name;
+          top: pose[poseID].top;
+          left: pose[poseID].left;
+      });
+    //removes pose from setOfPoses Array
+    setOfPoses.splice(poseID, 1);
+  }
+}
